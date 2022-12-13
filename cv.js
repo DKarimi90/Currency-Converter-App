2a11b6b11e7cae32f6f694a26f55d6956ae8a7bc
david <davmwa90@gmail.com>
2022-12-13 18:28:25 +0300

    Added event listeners to different variables

--- cv.js
+++ cv.js
@@ -3,3 +3,17 @@ const currencyElementOne = document.getElementById("currency-one");
 const currencyElementTwo = document.getElementById("currency-two"); 
 const amountElementOne = document.getElementById("amount-one"); 
 const amountElementTwo = document.getElementById("amount-two"); 
+
+const rateElement = document.getElementById("rate"); 
+const switchButton = document.getElementById("switch"); 
+
+currencyElementOne.addEventListener("change", calculate); 
+currencyElementTwo.addEventListener("change", calculate); 
+amountElementOne.addEventListener("input", calculate); 
+amountElementTwo.addEventListener("input", calculate); 
+
+
+
+
+
+
