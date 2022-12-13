126334765750f39a694ae705f5e5668afde7f722
david <davmwa90@gmail.com>
2022-12-13 18:10:15 +0300

    Defined different variables in the cv.js file

---
+++ cv.js
@@ -0,0 +1,5 @@
+// Defining different variables to use in this project 
+const currencyElementOne = document.getElementById("currency-one"); 
+const currencyElementTwo = document.getElementById("currency-two"); 
+const amountElementOne = document.getElementById("amount-one"); 
+const amountElementTwo = document.getElementById("amount-two"); 
