7c6854eeac305513123e9166b813efd36bece14c
david <davmwa90@gmail.com>
2022-12-13 18:47:57 +0300

    declared function and fetch url function

--- cv.js
+++ cv.js
@@ -7,10 +7,19 @@ const amountElementTwo = document.getElementById("amount-two");
 const rateElement = document.getElementById("rate"); 
 const switchButton = document.getElementById("switch"); 
 
+// Adding event listeners to various variables 
 currencyElementOne.addEventListener("change", calculate); 
 currencyElementTwo.addEventListener("change", calculate); 
 amountElementOne.addEventListener("input", calculate); 
 amountElementTwo.addEventListener("input", calculate); 
+// declaring the function and fetching data from the API 
+
+function calculate() {
+    const currencyOne = currencyElementOne.value;
+    const currencyTWO = currencyElementTwo.value;
+
+}
+calculate(); 
 
 
 
