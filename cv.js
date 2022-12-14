daf580d8e10d06ef6e6221a3e605bc8b634ab281
david <davmwa90@gmail.com>
2022-12-14 22:36:14 +0300

    Added color property to h4 earlier created using DOM manipulation

--- cv.js
+++ cv.js
@@ -6,12 +6,16 @@ const amountElementTwo = document.getElementById("amount-two");
 
 const rateElement = document.getElementById("rate"); 
 const switchButton = document.getElementById("switch"); 
+const submitButton = document.getElementById("sub")
+const element = document.createElement("h4"); 
+
 
 // Adding event listeners to various variables 
 currencyElementOne.addEventListener("change", calculate); 
 currencyElementTwo.addEventListener("change", calculate); 
 amountElementOne.addEventListener("input", calculate); 
 amountElementTwo.addEventListener("input", calculate); 
+
 // declaring the function and fetching data from the API 
 
 function calculate() {
@@ -39,8 +43,31 @@ function swap() {
     calculate()
 }
 swap(); 
+// changing inner text using DOM manipulation 
 let paragraph = document.querySelector("P")
 paragraph.innerHTML = "Choose Currency and Amount to Get Exchange Rates"
+
+// adding event listener to submit button 
+submitButton.addEventListener("click", process)
+function process(e) {
+    e.preventDefault(); 
+    return alert("Details Submitted")
+
+}
+process(); 
+
+// adding and appending element through DOM manipulation 
+document.body.appendChild(element); 
+element.innerHTML = "Login to enjoy the service"
+element.style.cssText = "font-family: times; text-align: center; font-weight: bold"; 
+const target = document.getElementById("contain"); 
+element.style.color = "#023e8a"
+document.body.insertBefore(element, target); 
+
+
+
+
+
 
 
 
